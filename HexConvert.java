8727e4df9dfe1aa59e630ad148dc6a55a1c686ce
bixiphel <allen131@rangers.uwp.edu>
2025-12-15 21:40:56 -0600

    Added file writing.

--- HexConvert.java
+++ HexConvert.java
@@ -1,23 +1,33 @@
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.File;
+import java.io.FileWriter;
 
 public class HexConvert {
 
 	public HexConvert() {}
 	
-	public void decode() {
+	public void decode(File infile, File outfile) {
 		try {
-			FileInputStream input = new FileInputStream("testImage.png");
+			// Sets up FileInputStream and FileWriter objects 
+			FileInputStream input = new FileInputStream(infile);
+			FileWriter writer = new FileWriter(outfile);
 			
+			// Holds the read in byte as an integer
 			int i;
 			
-			while ((i = input.read()) != -1) {
+			// Iterates through every character in the file until a character cannot be found, which FileInputStream.read() returns as -1.
+			while ((i = input.read()) != -1) {	
 				// Values are read in as integers, then converted to hexadecimal below
 				String hexValue = Integer.toHexString(i).toUpperCase();
-				String stringAsHex = (i < 17) ? "0" + hexValue : hexValue;
+				String stringAsHex = (i < 16) ? "0" + hexValue : hexValue;
 				
-				System.out.print(stringAsHex + " ");
+				// Prints out the current byte in hexadecimal format.
+				writer.write(stringAsHex + " ");
 			} 
+			
+			// Closes the FileWriter object, which technically isn't necessarily with the try-catch block but I wanted it here :p
+			writer.close();
 		} catch (IOException e) {
 				System.out.println("Error has occurred.");
 		} 	

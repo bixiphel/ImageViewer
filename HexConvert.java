912ec348a8ddebe555339158fac71740b706cad4
bixiphel <allen131@rangers.uwp.edu>
2025-12-19 01:09:22 -0600

    Refactored class to take an infile in the constructor and for decode() to return an ArrayList<String>.

--- HexConvert.java
+++ HexConvert.java
@@ -2,17 +2,28 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
 
+/**
+* Converts an inputted PNG file into its hexadecimal representation 
+*/
 public class HexConvert {
+	// Instance variables
+	private File infile;
 
-	public HexConvert() {}
+	// 1-arg constructor
+	public HexConvert(File infile) {
+		this.infile = infile;
+	}
 	
-	public void decode(File infile, File outfile) {
+	// Decodes the PNG file into its hexadecimal representation and returns an ArrayList
+	public ArrayList<String> decode() {
+		ArrayList<String> byteList = new ArrayList<String>();
+		
 		try {
 			// Sets up FileInputStream and FileWriter objects 
 			FileInputStream input = new FileInputStream(infile);
-			FileWriter writer = new FileWriter(outfile);
-			
+
 			// Holds the read in byte as an integer
 			int i;
 			
@@ -22,14 +33,40 @@ public class HexConvert {
 				String hexValue = Integer.toHexString(i).toUpperCase();
 				String stringAsHex = (i < 16) ? "0" + hexValue : hexValue;
 				
-				// Prints out the current byte in hexadecimal format.
-				writer.write(stringAsHex + " ");
-			} 
+				// Stores the converted hex value in the list
+				byteList.add(stringAsHex);
+			}			
+			return byteList;
 			
-			// Closes the FileWriter object, which technically isn't necessarily with the try-catch block but I wanted it here :p
-			writer.close();
 		} catch (IOException e) {
 				System.out.println("Error has occurred.");
-		} 	
+		}
+		
+		return byteList; 	
+	}
+	
+	// Decodes the PNG file into its hexadecimal representation and sends the output to a specified file
+	public void decode(File outfile) {		
+		try {
+			// Sets up FileInputStream and FileWriter objects 
+			FileInputStream input = new FileInputStream(infile);
+			FileWriter writer = new FileWriter(outfile);
+
+			// Holds the read in byte as an integer
+			int i;
+			
+			// Iterates through every character in the file until a character cannot be found, which FileInputStream.read() returns as -1.
+			while ((i = input.read()) != -1) {	
+				// Values are read in as integers, then converted to hexadecimal below
+				String hexValue = Integer.toHexString(i).toUpperCase();
+				String stringAsHex = (i < 16) ? "0" + hexValue : hexValue;
+				
+				// Prints out the current byte in hexadecimal format
+				writer.write(stringAsHex + " ");
+			}		
+			writer.close();				
+		} catch (IOException e) {
+				System.out.println("Error has occurred.");
+		}	
 	}
 }
